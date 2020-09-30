dba1c2433e6bfdd7e05e2aa41949b67846e17433
Apoorv <vaporinside@gmail.com>
2020-09-30 09:44:09 +0530

    CS50: Web /admin + authentications

--- CS50-web/database/project/airline/airline/urls.py
+++ CS50-web/database/project/airline/airline/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("flights/", include("flights.urls")),
+    path("users/", include("users.urls")),
     path('admin/', admin.site.urls),
 ]
