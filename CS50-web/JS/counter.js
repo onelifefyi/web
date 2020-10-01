58e8258f5402458b62fb6da7e92ea593c7e55c31
Apoorv <vaporinside@gmail.com>
2020-10-01 18:58:07 +0530

    CS50 JS Leftovers

--- CS50-web/JS/counter.js
+++ CS50-web/JS/counter.js
@@ -1,11 +1,16 @@
-let counter = 0;
+if(!localStorage.getItem('counter')){
+    localStorage.setItem('counter',0);
+}
+
 function count(){
+    let counter = localStorage.getItem('counter');
     counter++;
     document.querySelector('h1').innerHTML = counter;
-    if(counter % 5 === 0){
-        alert(`Count is now ${counter}`)
-    }
+    localStorage.setItem('counter', counter);
 }
 document.addEventListener('DOMContentLoaded', function(){
+    document.querySelector('h1').innerHTML = localStorage.getItem('counter');
     document.querySelector('button').onclick = count;
+
+    //setInterval(count, 1000);         // 1000ms = 1s
 })
