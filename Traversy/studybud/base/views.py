26957c6335a7f4f54a55c97f8a27b36d7e1652a3
Apoorv Chandrakar <apoorv@Apoorvs-MacBook-Air.local>
2021-12-22 20:22:11 +0530

    Django Project Templates and structures

---
+++ Traversy/studybud/base/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+rooms = [
+    {'id':1, 'name':'Lets Learn python!'},
+    {'id':2, 'name':'Design Patterns'},
+    {'id':3, 'name':'Memes!'},
+]
+
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = None
+    for obj in rooms:
+        if obj['id'] == int(pk):
+            room = obj
+
+    context = {'room': room}
+
+    return render(request, 'base/room.html', context)
